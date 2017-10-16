f544b50b8123b33e528dd2f1617a02f78f3476e9
Jörg Richter <jri@deepamehta.de>
2017-10-16 21:00:36 +0200

    Refactor ObjectRenderer template

--- src/components/ObjectRenderer.vue
+++ src/components/ObjectRenderer.vue
@@ -1,32 +1,45 @@
 <template>
-  <div class="object-renderer" v-if="object">
+  <div class="object-renderer">
     <!-- simple -->
     <div v-if="isSimple" class="field">
       <div class="field-label">{{label}}</div>
       <component :is="simpleComp" v-model="object.value" :mode="mode"></component>
     </div>
     <!-- composite -->
-    <div v-else v-for="assocDef in assocDefs" :key="assocDef.id">
+    <template v-else v-for="assocDef in assocDefs">
       <!-- one -->
-      <object-renderer v-if="isOne(assocDef)" :object="childs(assocDef)" :mode="mode">
-      </object-renderer>
+      <template v-if="isOne(assocDef)">
+        <object-renderer v-if="childs(assocDef)" :object="childs(assocDef)" :mode="mode">
+        </object-renderer>
+      </template>
       <!-- many -->
       <object-renderer v-else v-for="object in childs(assocDef)" :object="object" :mode="mode" :key="object.id">
       </object-renderer>
-    </div>
+    </template>
   </div>
 </template>
 
 <script>
+import dm5 from 'dm5'
+
 export default {
 
   name: 'object-renderer',
 
-  props: [
-    'object',   // the Topic/Assoc to render; is never undefined;
-                // may be an "empty" topic/assoc, without ID, with just type set
-    'mode'      // 'info' or 'form'
-  ],
+  props: {
+    // the Topic/Assoc to render; is never undefined;
+    // may be an "empty" topic/assoc, without ID, with just type set
+    object: {
+      type: dm5.DeepaMehtaObject,
+      required: true
+    },
+    // 'info' or 'form'
+    mode: {
+      type: String,
+      required: true,
+      validator: mode => mode === 'info' || mode === 'form'
+    }
+  },
 
   computed: {
 
