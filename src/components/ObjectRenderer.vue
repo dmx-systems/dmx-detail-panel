2f33c987aebe751aaaeafbb9c4569804d1544628
Jörg Richter <jri@deepamehta.de>
2017-12-12 16:11:49 +0100

    Form: button for adding value to multi-value

--- src/components/ObjectRenderer.vue
+++ src/components/ObjectRenderer.vue
@@ -2,7 +2,7 @@
   <div :class="['object-renderer', localMode]" @click.stop="editInline">
     <!-- simple -->
     <div v-if="isSimple" class="field simple">
-      <div class="field-label">{{label}}</div>
+      <div class="field-label">{{simpleLabel}}</div>
       <div class="field-content">
         <component :is="simpleRenderer" :object="object" :mode="localMode" :assoc-def="assocDef"></component>
         <el-button class="save-button" v-if="inlineEdit" @click.stop="submitInline">Save</el-button>
@@ -17,9 +17,14 @@
         </child-topic>
       </template>
       <!-- many -->
-      <child-topic v-else v-for="child in childs(assocDef)" :object="child" :mode="mode" :level="level+1"
-        :assoc-def="assocDef" :key="child.id">
-      </child-topic>
+      <template v-else>
+        <child-topic v-for="child in childs(assocDef)" :object="child" :mode="mode" :level="level+1"
+          :assoc-def="assocDef" :key="child.id">
+        </child-topic>
+        <el-button v-if="formMode" class="add-button" icon="el-icon-plus" :title="addButtonTitle(assocDef)"
+          @click="addChild(assocDef)">
+        </el-button>
+      </template>
     </template>
   </div>
 </template>
@@ -46,17 +51,13 @@ export default {
       return this.object.getType()
     },
 
-    label () {
-      const customAssocType = this.assocDef && this.assocDef.getCustomAssocType()
-      return customAssocType && customAssocType.isSimple() ? customAssocType.value : this.type.value
-    },
-
     isSimple () {
       return this.type.isSimple()
     },
 
-    assocDefs () {
-      return this.type.assocDefs
+    simpleLabel () {
+      const customAssocType = this.assocDef && this.assocDef.getCustomAssocType()
+      return customAssocType && customAssocType.isSimple() ? customAssocType.value : this.type.value
     },
 
     simpleRenderer () {
@@ -70,6 +71,10 @@ export default {
 
     inlineEdit () {
       return this.$store.state.detailPanel.inlineCompId === this._uid   // FIXME: _uid is Vue internal
+    },
+
+    assocDefs () {
+      return this.type.assocDefs
     }
   },
 
@@ -92,6 +97,17 @@ export default {
 
     submitInline () {
       this.$store.dispatch('submitInline')
+    },
+
+    // add value
+
+    addButtonTitle (assocDef) {
+      const type = assocDef.getCustomAssocType()
+      return `Enter another ${type ? type.value : assocDef.getChildType().value}`
+    },
+
+    addChild (assocDef) {
+      this.childs(assocDef).push(assocDef.emptyChildInstance())
     },
 
     //
@@ -130,5 +146,12 @@ export default {
 
 .object-renderer .field .field-content .save-button {
   margin-left: 1em;
+}
+
+.object-renderer .add-button {
+  font-size: var(--label-font-size) !important;
+  padding: 3px;
+  position: absolute;
+  right: 0;
 }
 </style>
