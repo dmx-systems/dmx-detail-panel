87dce24c0d550ae2952c04c767c4857df53e6504
Jörg Richter <jri@deepamehta.de>
2019-06-04 16:42:25 +0200

    Adapt "assoc def" -> "comp def" wording in messages

--- src/components/dm5-detail-panel.vue
+++ src/components/dm5-detail-panel.vue
@@ -75,12 +75,12 @@ export default {
       if (this.object_ && (this.object_.isType() || this.object_.isCompDef())) {
         const viewConfig = this.object_.viewConfig
         if (!viewConfig) {
-          console.warn('Type or assoc def has no view config', this.object_)
+          console.warn('Type or comp def has no view config', this.object_)
           return
         }
         const viewConfigTopic = viewConfig['dmx.webclient.view_config']
         if (!viewConfigTopic) {
-          console.warn('Type or assoc def has no view config topic', this.object_)
+          console.warn('Type or comp def has no view config topic', this.object_)
           return
         }
         return viewConfigTopic
