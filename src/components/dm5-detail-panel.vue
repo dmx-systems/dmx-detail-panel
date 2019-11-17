0ea87a5c52ccec17a917dee89627e0b53c4435c6
Jörg Richter <jri@deepamehta.de>
2019-11-17 09:30:59 +0100

    Add `no-pub-button` attr to suppress pin button
    See dmx-platform/dmx-platform#327

--- src/components/dm5-detail-panel.vue
+++ src/components/dm5-detail-panel.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="dm5-detail-panel" v-if="visible_"><!-- background is already shown for the sake of feedback -->
-    <el-button :class="['pin', {unpinned: !pinned_}, 'fa', 'fa-thumb-tack']" type="text" :title="pinTitle"
-      @click="togglePinned">
+    <el-button v-if="!noPinButton" :class="['pin', {unpinned: !pinned_}, 'fa', 'fa-thumb-tack']" type="text"
+      :title="pinTitle" @click="togglePinned">
     </el-button>
     <el-tabs v-if="object_" :value="tab_" @tab-click="tabClick"><!-- tabs are shown once object arrives -->
       <el-tab-pane :label="object_.typeName" name="info">
@@ -55,7 +55,8 @@ export default {
     object: dm5.DMXObject,                      // The topic/assoc to display. Undefined if data not yet available.
     markerIds: Array,                           // Optional: IDs of topics to render as "marked" in related-tab.
     types: Object,                              // Optional: "assocTypes" and "roleTypes" (arrays)
-    quillConfig: Object
+    quillConfig: Object,
+    noPinButton: Boolean
   },
 
   data () {
