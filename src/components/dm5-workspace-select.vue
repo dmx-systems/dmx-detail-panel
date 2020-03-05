3043d64b0691ef998940c3c9ebca589629ab13fa
Jörg Richter <jri@deepamehta.de>
2020-03-05 01:50:37 +0100

    Meta tab: disable non-writable workspaces
    See dmx-platform/dmx-platform#340

--- src/components/dm5-workspace-select.vue
+++ src/components/dm5-workspace-select.vue
@@ -1,19 +1,19 @@
 <template>
   <el-select class="dm5-workspace-select" v-model="workspace_">
-    <el-option v-for="workspace in workspaces" :label="workspace.value" :value="workspace" :key="workspace.id">
+    <el-option v-for="workspace in workspaces" :label="workspace.value" :value="workspace"
+      :disabled="!workspace.writable" :key="workspace.id">
     </el-option>
   </el-select>
 </template>
 
 <script>
 import dm5 from 'dm5'
+import Vue from 'vue'
 
 export default {
 
   created () {
-    dm5.restClient.getTopicsByType('dmx.workspaces.workspace').then(workspaces =>
-      this.workspaces = workspaces
-    )
+    this.initWorkspaces()
   },
 
   props: {
@@ -22,8 +22,22 @@ export default {
 
   data () {
     return {
-      workspaces: [],
-      workspace_: this.workspace
+      workspaces: [],               // all workspaces (array of dm5.Topic)
+      workspace_: this.workspace    // selected workspace (dm5.Topic)
+    }
+  },
+
+  methods: {
+    initWorkspaces () {
+      dm5.restClient.getTopicsByType('dmx.workspaces.workspace').then(workspaces => {
+        this.workspaces = workspaces
+        // set writable flags
+        workspaces.forEach(workspace => {
+          workspace.isWritable().then(writable => {
+            Vue.set(workspace, 'writable', writable)
+          })
+        })
+      })
     }
   }
 }
